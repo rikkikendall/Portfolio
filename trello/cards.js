9c41cf915a4c9a25075bb150021e412414fd3c2e
Rikki Kendall <yrk3@duke.edu>
2018-03-19 23:21:02 -0400

    Add files via upload

--- trello/cards.js
+++ trello/cards.js
@@ -1,3 +1,8 @@
+Vue.directive('focus', {
+  componentUpdated: function (el) {
+    el.focus();
+  }
+});
 var config = {
     apiKey: "AIzaSyBEPme4kqo6xH8RRDbr2dNofVLfA1a4g9M",
     authDomain: "cs290-cards.firebaseapp.com",
@@ -8,180 +13,159 @@ var config = {
 };
 firebase.initializeApp(config);
 var database = firebase.database();
-var rootRef = firebase.database().ref();
-var listRef = rootRef.child("lists").push();
+var rootRef = database.ref();
 
-var data;
+//var fb = firebase.initializeApp(config).database();
+////var datab = fb.database();
 
-var data = {
-    "stack" : {
-        "stack1": {
-            "name": "Marvin Bagley III",
-            "lists": [
-                {
-                    "title": "Baseball",
-                    "cards": [
-                        {
-                            "title": "Ichiro Suzuki",
-                            "description": "Right Field - Bats Left",
-                            "deadline": "3/9/18",
-                            "shouldDisplay": false
-                        },
-                        {
-                            "title": "Yu Darvish",
-                            "description": "Pitcher - Bats Right",
-                            "deadline": "3/1/18",
-                            "shouldDisplay": false
-                        }
-                    ]
-                },
-                {
-                    "title": "Basketball",
-                    "cards": [
-                        {
-                            "title": "Marvin Bagley III",
-                            "description": "PF/C",
-                            "deadline": "3/9/18",
-                            "shouldDisplay": false
-                        },
-                        {
-                            "title": "Gary Trent Jr",
-                            "description": "SG",
-                            "deadline": "3/1/18",
-                            "shouldDisplay": false
-                        }
-                    ]
-                }
-            ]
-        }
-    }
-};
+////var listRef = rootRef.child('lists').push();
+////REFERENCES for Use
+//var stackRef = rootRef('lists');
+//var usersRef = rootRef('users');
+//var imagesRef = rootRef('imgs');
 
-function addListDisp() {
-    var modal = document.getElementById('addList');
-    modal.style.display = 'block';
-}
+//var storageRef = firebase.storage().ref();
+//Vue Connect Firebase
+//var VueFire;
+//var Vue = Vue.use(VueFire);
 
-function addListClose() {
-    var modal = document.getElementById('addList');
-    modal.style.display = 'none';
-}
+//var menu = document.getElementById("change_chart");
+//menu.addEventListener("change", options);
+//function optionsSelect(val) {
+//    addCard();
+//    if (val > 0) {
+//        console.log("HELLO"+val);
+//    }
+//    if (menu.value == '1') {
+//        //do something
+//        alert(1)
+//    } else if (menu.value == '2') {
+//    //do something
+//    alert(2)
+//  }  else if (menu.value == '3') {
+//    //do something
+//    alert(3)
+//  }
 
-function addList() {
-    var content = document.getElementById('listcontent');
-    // MAKE SURE CONTENT IS THE ACTUAL TITLE
-    var newList = {"title": content, "cards": []};
-    listRef.set(newList);
-    listRef.update({
-        path: listRef.ref.toString()
-    });
-    var modal = document.getElementById('addList');
-    if (content === "") {
-        modal.style.display = 'none';
-        alert("WHAT");
-    }
-    else{
-        data.stack.stack1.lists.push(newList);
-        modal.style.display = 'none';
-    }
-}
-
-function getrid(){
-    var content = document.getElementById('listgrcontent');
-    var lists = data.stack.stack1.lists;
-    var k;
-    var modal = document.getElementById('getrid')
-    for(k=lists.length-1; k>=0; k--){
-        console.log(lists[k]);
-        console.log(lists[k].title);
-        if(lists[k]==content){
-            lists.splice(k,1);
-        }
-    }
-    modal.style.display = 'none';
-}
-
-function seeNewCard(index){
-    var modal = document.getElementById('addcard'+index);
-    modal.style.display= 'block';
-}
-
-function closethat(index){
-    var modal = document.getElementById('addcard'+index);
-    modal.style.display= 'none';
-}
-
-function NewCard(list,index){
-    var content = document.getElementById('cardcontent'+index).content;
-    var newCard = {"title":content, "description":"", "deadline":"", shouldDisplay:false};
-    var cardRef = YOURLISTREF.ref("cards").push().set(newCard);
-    var modal = document.getElementById('addcard'+index);
-    if(content===""){
-        alert("");
-    }
-    else{
-        lists.cards.push(newCard);
-    }
-    modal.style.display = 'none';
-}
-
-function seeRemoveCard(){
-    var modal = document.getElementById('removecard'+index);
-    modal.style.display= 'block';
-}
-
-function closethat2(index){
-    var modal = document.getElementById('removecard'+index);
-    modal.style.display= 'none';
-}
-
-function getridCard(list,index){
-    var content = document.getElementById('cardgrcontent');
-    var lists = data.stack.stack1.lists;
-    var k;
-    var modal = document.getElementById('getridcard')
-    if(content!==null){
-        for(k=list.cards.length-1; k>=0; k--){
-            if(lists[k].title==content){
-            lists.cards.splice(k,1);
-        }
-        }
-        modal.style.display='none';
-    }
-    else{
-        modal.style.display = 'none';
-        alert("");
-    }
-}
-
-function closeModals(){
-	console.log("closing")
-	var k;
-	for(k=0; k<data.stack.stack1.lists.length; k++){
-		var i;
-		for(i=0; i<data.stack.stack1.lists[i].cards.length; i++){
-			data.stack.stack1.lists[i].cards[j].shouldDisplay=false;
-		}
-	}
-}
 
 //const app = new Vue({
-var app = new Vue({
-//    el: '#app',
-    data: {
-//        keyword: 'javascript'
-        stack: data.stack.stack1,
-        newList:"",
-        newCard:"",
-    },
-    methods:{
-        displayMod:function(card){
-            card.shouldDisplay = true;
-        },
-        closeabove:function(card){
-            card.shouldDisplay = false;
-        }
-    }
-})
 
-app.$mount('#app');
+    var app = new Vue({
+    el: "#app",
+    data: {
+        cards: [],
+        users: [],
+        lists: [],
+        Username: false,
+        Name: "",
+        Description: "",
+        Email: "",
+        login: false,
+        horizontal: false,
+        See: null,
+        ListName: "",
+        ChangeListName: null,
+        ChangeCard: false,
+        ChangeDueDate: false,
+        CardClicked: null,
+        ChangeCardDescription: false,
+        
+        startscreen: true,
+        PictureName: '',
+
+        hide: false,
+//        newInLi: '',
+        Categories: [
+            { text: 'Categories'},
+            { text: 'Work'},
+            { text: 'Play'},
+            { text: 'Important'},
+            { text: 'Personal'},
+        ]
+    },
+//        firebase: {
+//        cards: stackRef,
+//        users: usersRef,
+//        images: imagesRef
+//    },
+    methods: {
+    	addToDatabase: function(key, data){
+            database.ref("/"+key).set(data);
+        },
+    	ListTitle: function() {
+        	this.ChangeListName = null;
+        	this.addToDatabase("lists", this.lists);
+        },
+        CardDescription: function() {
+        	this.ChangeCardDescription = false;
+        	this.addToDatabase("lists", this.lists);
+        },
+        CardItself: function() {
+        	this.ChangeCard = false;
+        	this.addToDatabase("lists", this.lists);
+        },
+        CardDueDate: function() {
+        	this.ChangeDueDate = false;
+        	this.addToDatabase("lists", this.lists);
+        },
+
+        showInput: function(input) {
+        input.isInputVisible = true;
+        },
+
+//        onEnterClicked: function() {
+//            alert();
+//        },
+        PopUp: function(card) {
+      		this.CardClicked = card;
+    	},
+    	NewUser: function() {
+      			this.username = false;
+      			this.addToDatabase("users", this.users);
+    	},
+        newCard: function(list) {
+          	list.cards.push({
+//            id: Date().getDay() + "/" + Date().getMonth() + "/" + Date().getFullYear(),
+            id: Date.now(),
+            Name: list.ChangeText,
+            Description: "Click to edit description",
+            DueDate: "Click to add a due date",
+            Time: Date.now(),
+//            Username: this.username,
+//            Email: this.email,
+            Category: "uncategorized",
+            stack: []
+          });
+        //console.log("yeah");
+          list.ChangeText = ""; // clear text
+          this.addToDatabase("lists", this.lists);
+        },
+        newList: function() {
+            this.lists.push({
+//            id: Date().getDay + "/" + Date().getMonth + "/" + Date().getFullYear,
+            id: Date.now(),
+            title: this.ListName,
+            cards: []
+        });
+        this.addToDatabase("lists", this.lists);
+        this.ListName = "";
+        },
+        addCard: function(card) {
+            if (!("stack" in card)) { 
+                    Vue.set(card, "stack", []); 
+                }
+                card.stack.push(this.todoText);
+                this.todoText = ""; // clear input
+                this.addToDatabase("stack", this.lists);
+            }
+        },
+        storeImage: function() {
+            var input = document.getElementById('files');
+            if (input.files.length > 0) {
+          		Vue.set(this.currentCard, "images", []);
+            }
+        }
+    
+});
+//app.$mount('#app');
+
